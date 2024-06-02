0b59cf217c3d34a4411d987f9c14e88d7115d804
Rashed <w3rashed@gmail.com>
2024-06-02 18:27:04 +0600

    create user operation added

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// -------------------------------------------------
+
+const uri = "mongodb://localhost:27017";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrpddgz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("bistroDb").collection("users");
+
+    app.post("/users", async (re, res) => {
+      const user = req.body;
+      // checking not exist email
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+// ------------------------------------------------
+
+app.get("/", (req, res) => {
+  res.send("asset nex is running");
+});
+
+app.listen(port, () => {
+  console.log(`Asset nex is sitting on port ${port}`);
+});
